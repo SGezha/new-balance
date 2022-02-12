6aa5c92537742ec259a82246d9cf5d2419027107
SGezha <sasha-gezha1@mail.ru>
2022-02-12 17:06:18 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ command, mode }) => {
   console.log('🦕 vite.config.js/defineConfig', command, mode)
 
   return {
+    base: './',
     resolve: {
       alias: {
         '@/': `${path.resolve(__dirname, 'src')}/`,
